7b54d5984cbcf2a08afbfbd0056d5ff8c8130390
Dana <danamudrova@gmail.com>
2022-04-08 15:20:32 +0200

    ovoce a kalorie

--- main.js
+++ main.js
@@ -6,6 +6,25 @@ hodnotu potraviny na obrázku (číslo si klidně vymysli).
 Zařiď, aby se při kliknutí na obrázek volala funkce,
 která vypíše kalorickou hodnotu potraviny.
 
+(click)
+
 BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+
+
+
+const obrazky = document.querySelectorAll('.obrazky')
+
+
+obrazky.forEach((obrazky) => {
+    obrazky.addEventListener('click', vypisKalorie)
+})
+
+
+
+function vypisKalorie(udalost) {
+    let obrazek = udalost.target;
+    let kalorie = obrazek.dataset.ovoce;
+    document.querySelector('#vysledek').textContent = kalorie;
+}
